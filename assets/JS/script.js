154c2f9d51144683fc91ddecfac014c605e53163
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-01-29 20:46:10 +0100

    fixed the problem  with the rendering and feat the methods

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,52 +1,55 @@
-const {createApp} = Vue
+const {createApp} = Vue 
 
 createApp({
     data(){
         return {
             currentImage: 0,
-
             slides: [
                 {
-                    image: 'img/01.webp',
+                    image: './assets/img/01.webp',
                     title: 'Marvel\'s Spiderman Miles Morale',
                     text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-                }, {
-                    image: 'img/02.webp',
+                }, {           
+                    image: './assets/img/02.webp',
                     title: 'Ratchet & Clank: Rift Apart',
                     text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
                 }, {
-                    image: 'img/03.webp',
+                    image: './assets/img/03.webp',
                     title: 'Fortnite',
                     text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
                 }, {
-                    image: 'img/04.webp',
+                    image: './assets/img/04.webp',
                     title: 'Stray',
                     text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
                 }, {
-                    image: 'img/05.webp',
+                    image: './assets/img/05.webp',
                     title: "Marvel's Avengers",
                     text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
                 }
-            ]
+            ] 
         }
     },
 
     methods: {
-        chevUp() {
-            if (currentImage === 0){
-                currentImage = slides.length-1
-        
-            } else {
-                currentImage--
+        prevImage(){
+            this.currentImage--
+      
+            if( this.currentImage < 0 ){
+              this.currentImage = this.slides.length - 1
             }
-        }, 
+      
+        },
 
-        chevDown() {
-            if(currentImage === slides.length-1){
-                currentImage = 0
-            } else {
-                currentImage++
+        nextImage(){
+            this.currentImage++
+      
+            if( this.currentImage > this.slides.length - 1  ){
+              this.currentImage = 0
             }
+        },
+        
+        alternativeImage(index){
+            this.currentImage = index
         }
     }
 }).mount("#app")
